546b05cee6875612e5bbd7be5cd9e82bfa14a14b
Mika Tervonen <mtervonen80@gmail.com>
2020-12-25 15:15:45 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const minifyCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
-const browserSync = require('browser-sync').create();
 const del = require('del');
 
 gulp.task('clean', function () {
     return del([
-        'build/*css', 'build/dist/*css'
+        'build/*css', 'dist/*css'
     ]);
 });
 
@@ -32,28 +31,14 @@ gulp.task('minify', function () {
             path.extname = ".min.css";
         }))
         // minify css output directory
-        .pipe(gulp.dest('./build/dist'))
-});
-
-
-// sass/css browser tracking
-gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
+        .pipe(gulp.dest('./dist'))
 });
 
 // gulp default (clean, sass, minify-css) method
 gulp.task('default', gulp.series(['clean', 'sass', 'minify']));
-gulp.task('serve', gulp.series('browser-sync'));
+
 gulp.task('watch', function () {
-    //gulp.watch('./src/*.scss').on('change','sass');
-    //gulp.watch('./build/*.css').on('change','minify-css');
-    // Other watchers
     gulp.series(['clean', 'sass', 'minify'])
     gulp.watch("src/*.scss").on('change', gulp.series(['clean', 'sass']));
     gulp.watch("build/*.css").on('change', gulp.series('minify'));
-    gulp.watch('./*.html').on('change', browserSync.reload);
 })
